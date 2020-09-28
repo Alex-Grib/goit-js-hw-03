d2aa67e367bf1b1041b5dc4a6a3bb0864b0bc1b4
Alexandr Grib <alexgribaldi@gmail.com>
2020-09-28 19:01:59 +0200

    добавляет задачу

---
+++ js/test.js
@@ -0,0 +1,29 @@
+const hotel = {
+  name: "Resort Hotel",
+  stars: 5,
+  capacity: 100,
+  personel: 20,
+};
+
+console.log(hotel);
+
+const name = "alex";
+const secondName = "grib";
+
+let balance = 2000;
+const result = 10 >= balance || balance < 1999;
+let product = 5;
+let message;
+console.log(result);
+console.log(name);
+console.log(secondName);
+
+const alexGrib = {
+  name: "alex",
+  surename: "Grib",
+  age: 27,
+  gender: "male",
+};
+
+console.log(alexGrib);
+console.log(!false);
