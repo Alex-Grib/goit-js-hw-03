ff7cc08f3efe0618608de86260fc3460fdc7231d
Alexandr Grib <alexgribaldi@gmail.com>
2020-09-30 15:30:26 +0200

    test

--- js/test.js
+++ js/test.js
@@ -1,29 +1,6 @@
-const hotel = {
-  name: "Resort Hotel",
-  stars: 5,
-  capacity: 100,
-  personel: 20,
-};
-
-console.log(hotel);
-
-const name = "alex";
-const secondName = "grib";
-
-let balance = 2000;
-const result = 10 >= balance || balance < 1999;
-let product = 5;
-let message;
-console.log(result);
-console.log(name);
-console.log(secondName);
-
-const alexGrib = {
-  name: "alex",
-  surename: "Grib",
-  age: 27,
-  gender: "male",
-};
-
-console.log(alexGrib);
-console.log(!false);
+let salary = 2650.552334223;
+salary = salary.toFixed(2);
+salary = Number(salary);
+salary = Number(salary.toFixed(2));
+console.log(salary);
+console.log(Number(salary.toFixed(2)));
